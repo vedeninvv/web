2fe24200a1bc45984d8ed8aa3b6240495c4c6138
Barabashka <vedeninvv@mail.ru>
2021-11-04 11:36:23 +0300

    add pages for flats and neighbors

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+(function() {
+    let page = document.location.toString()
+
+    let menu = document.getElementsByClassName("menu")[0]
+    let menuOptions = menu.getElementsByTagName("a")
+    for (let i = 0; i < menuOptions.length; i++) {
+        if (menuOptions[i].href === page){
+            menuOptions[i].classList.add("menu__item_active");
+        }
+    }
+})()
