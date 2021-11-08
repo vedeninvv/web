b7648378ed21c5a7fe78827babfd368691ec5577
Barabashka <vedeninvv@mail.ru>
2021-11-08 15:42:06 +0300

    fix timeLoad

--- js/pageLoadTime.js
+++ js/pageLoadTime.js
@@ -1,9 +1,11 @@
-var beforLoad = new Date().getTime();
+// var beforLoad = new Date().getTime();
 window.onload = loadTime;
 
-function loadTime () {
-    let afterLoad = new Date().getTime();
-    let pageLoadTime = (afterLoad - beforLoad) / 1000
+function loadTime() {
+//    let afterLoad = new Date().getTime();
+//     let pageLoadTime = (window.performance.timing.domContentLoadedEventEnd - window.performance.timing.connectStart) / 1000
+    let pageLoadTime = (PerformanceNavigationTiming.prototype.domContentLoadedEventEnd
+        - PerformanceNavigationTiming.prototype.connectStart) / 1000;
     let footer = document.getElementsByClassName("footer").item(0)
     let pageLoadTimeTextLine = document.createElement("p")
     let pageLoadTimeText = document.createTextNode("Time load: " + pageLoadTime.toString() + " s.")
