0ffe821f60a7da94d0da53f38243d378721b1b7f
Barabashka <vedeninvv@mail.ru>
2021-11-08 15:34:34 +0300

    fix menu

--- js/menu.js
+++ js/menu.js
@@ -4,7 +4,7 @@
     let menu = document.getElementsByClassName("menu")[0]
     let menuOptions = menu.getElementsByTagName("a")
     if (document.location.href === "https://vedeninvv.github.io/web/"){
-        menuOptions[i].classList.add("menu__item_active");
+        menuOptions[0].classList.add("menu__item_active");
         return;
     }
     for (let i = 0; i < menuOptions.length; i++) {
