b4c5fe57229d628c30729c8d7f27541978f45250
Barabashka <vedeninvv@mail.ru>
2021-11-08 15:12:48 +0300

    fix menu

--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,6 @@
 
     let menu = document.getElementsByClassName("menu")[0]
     let menuOptions = menu.getElementsByTagName("a")
-    alert(page)
     for (let i = 0; i < menuOptions.length; i++) {
         if (menuOptions[i].href === page){
             menuOptions[i].classList.add("menu__item_active");
